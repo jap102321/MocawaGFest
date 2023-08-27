8132676247c5e00ed96b10ccf1ba3345b0dec107
Juan Pablo Gonzalez <jupago14@gmail.com>
2023-08-26 20:02:13 -0500

    removing buttons

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import styles from './page.module.css';
 import { Montserrat } from 'next/font/google';
-
+import './globals.css';
 const montse = Montserrat({ subsets: ['latin'] });
 
 export const metadata = {
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={`${styles.backgroundContainer}`}>{children}</body>
+      <body className={`${styles.backgroundContainer} ${montse.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,18 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import Button from './components/Button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
-      <div className={styles.buttonContTop}>
-        <Button />
-      </div>
-      <div className={styles.buttonContBot}>
+      <Link href='#seeMore'>
+        <button className={styles.goDownBtn}>
+          <h3>Ver más información ⬇️</h3>
+        </button>
+      </Link>
+
+      <div id='seeMore' className={styles.main_button}>
         <Button />
       </div>
     </>
