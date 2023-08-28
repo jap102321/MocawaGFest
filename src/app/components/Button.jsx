eda36f819e2b9bcf635adca75b1f7f47514d82de
Juan Pablo Gonzalez <jupago14@gmail.com>
2023-08-28 10:30:02 -0500

    Responsiveness

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -3,7 +3,7 @@ import styles from './Button.module.css';
 const Button = () => {
   return (
     <button className={styles.button}>
-      <a href='#'>
+      <a href='https://api.whatsapp.com/send/?phone=573136449825&text&type=phone_number&app_absent=0'>
         <h2>PULSA PARA</h2>
         <h3>MAS INFORMACIÓN</h3>
       </a>
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,10 @@
 'use client';
-import Image from 'next/image';
 import styles from './page.module.css';
 import Button from './components/Button';
-import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
-      <Link href='#seeMore'>
-        <button className={styles.goDownBtn}>
-          <h3>Ver más información ⬇️</h3>
-        </button>
-      </Link>
-
       <div id='seeMore' className={styles.main_button}>
         <Button />
       </div>
